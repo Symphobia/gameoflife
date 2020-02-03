427f6576803249bd438dafeb258b3d9e37da5dac
Symphobia <muhsin.bayram@outlook.com>
2020-02-03 16:57:19 +0300

    Updates

---
+++ precache-manifest.eddb45370f3e17a07c3b3cc2ea4b75d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be8c65a5c9f978c59374e12e3c7708fd",
+    "url": "/gameoflife/index.html"
+  },
+  {
+    "revision": "c9a8096957346ad6c20d",
+    "url": "/gameoflife/static/css/main.b543d147.chunk.css"
+  },
+  {
+    "revision": "59b672372679bbca01ac",
+    "url": "/gameoflife/static/js/2.4c3bb888.chunk.js"
+  },
+  {
+    "revision": "c9a8096957346ad6c20d",
+    "url": "/gameoflife/static/js/main.9fc324b3.chunk.js"
+  },
+  {
+    "revision": "4d94fa84d76ce9b3b434",
+    "url": "/gameoflife/static/js/runtime~main.bd3f78c1.js"
+  }
+]);
